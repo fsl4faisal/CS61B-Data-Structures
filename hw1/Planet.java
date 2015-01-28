2ff88cbfd3310eb9efc6c1bca21f61d41e8c11ba
Hubert <hubqwerty@berkeley.edu>
2015-01-28 00:30:58 -0800

    First Planet.java

---
+++ hw1/Planet.java
@@ -0,0 +1,17 @@
+public class Planet {
+	public int x;
+	public int y;
+	public int xVelocity;
+	public int yVelocity;
+	public int mass;
+	public String img;
+
+	public Planet(double x, double y, double xVelocity, double yVelocity, double mass, String img) {
+		this.x = x
+		this.y = y
+		this.xVelocity = xVelocity
+		this.yVelocity = yVelocity
+		this.mass = mass
+		this.img = img
+	}
+}
