07e26577718f13cf57cd60227c65bf149f13f8f0
Hubert Pham <hubqwerty@berkeley.edu>
2015-03-11 21:37:41 -0700

    porj 1

--- proj1/ngordnet/NGramMap.java
+++ proj1/ngordnet/NGramMap.java
@@ -6,154 +6,154 @@ import java.util.Iterator;
 import java.util.Map;
 import java.util.HashMap;
 public class NGramMap {
-	private String wordsFile;
-	private String countsFile;
-	private TimeSeries<Long> counts;
-	private Map<Integer, YearlyRecord> years;
-	/** Constructs an NGramMap from WORDSFILENAME and COUNTSFILENAME. */
-	public NGramMap(String wordsFilename, String countsFilename) {
-		this.wordsFile = wordsFilename;
-		this.countsFile = countsFilename;
-		counts = new TimeSeries<Long>();
-		years = new HashMap<Integer, YearlyRecord>();
-		readWords(wordsFile);
-		readCounts(countsFile);
-	}
+    private String wordsFile;
+    private String countsFile;
+    private TimeSeries<Long> counts;
+    private Map<Integer, YearlyRecord> years;
+    /** Constructs an NGramMap from WORDSFILENAME and COUNTSFILENAME. */
+    public NGramMap(String wordsFilename, String countsFilename) {
+        this.wordsFile = wordsFilename;
+        this.countsFile = countsFilename;
+        counts = new TimeSeries<Long>();
+        years = new HashMap<Integer, YearlyRecord>();
+        readWords(wordsFile);
+        readCounts(countsFile);
+    }
 
-	private void readWords(String filename) {
-		In reader = new In(filename);
-		while (reader.hasNextLine()) {
-			String[] read = reader.readLine().split("\t");
-			String str = read[0];
-			Integer year = Integer.valueOf(read[1]);
-			Integer count = Integer.valueOf(read[2]);
-			if (years.get(year) == null) {
-				YearlyRecord y = new YearlyRecord();
-				y.put(str, count);
-				years.put(year, y);
-			}
-			else {
-				years.get(year).put(str, count);
-			}
-		}
-	}
+    private void readWords(String filename) {
+        In reader = new In(filename);
+        while (reader.hasNextLine()) {
+            String[] read = reader.readLine().split("\t");
+            String str = read[0];
+            Integer year = Integer.valueOf(read[1]);
+            Integer count = Integer.valueOf(read[2]);
+            if (years.get(year) == null) {
+                YearlyRecord y = new YearlyRecord();
+                y.put(str, count);
+                years.put(year, y);
+            }
+            else {
+                years.get(year).put(str, count);
+            }
+        }
+    }
 
-	private void readCounts(String filename) {
-		In reader = new In(filename);
-		while (reader.hasNextLine()) {
-			String[] read = reader.readLine().split(",");
-			Integer year = Integer.valueOf(read[0]);
-			Long count = Long.valueOf(read[1]);
-			counts.put(year, count);
-		}
-	}
-	
-	/** Returns the absolute count of WORD in the given YEAR. If the word
-	  * did not appear in the given year, return 0. */
-	public int countInYear(String word, int year) {
-		YearlyRecord words = years.get(year);
-		Collection<String> keys = words.words();
-		Iterator<String> kIter = keys.iterator(); 
-		while (kIter.hasNext()) {
-			if (kIter.next().equals(word)) {
-				return words.count(word);
-			}
-		}
-		return 0;
-	}
-	
+    private void readCounts(String filename) {
+        In reader = new In(filename);
+        while (reader.hasNextLine()) {
+            String[] read = reader.readLine().split(",");
+            Integer year = Integer.valueOf(read[0]);
+            Long count = Long.valueOf(read[1]);
+            counts.put(year, count);
+        }
+    }
+    
+    /** Returns the absolute count of WORD in the given YEAR. If the word
+      * did not appear in the given year, return 0. */
+    public int countInYear(String word, int year) {
+        YearlyRecord words = years.get(year);
+        Collection<String> keys = words.words();
+        Iterator<String> kIter = keys.iterator(); 
+        while (kIter.hasNext()) {
+            if (kIter.next().equals(word)) {
+                return words.count(word);
+            }
+        }
+        return 0;
+    }
+    
 
-	/** Returns a defensive copy of the YearlyRecord of YEAR. */
-	public YearlyRecord getRecord(int year) {
-		YearlyRecord result = new YearlyRecord();
-		YearlyRecord all_years = years.get(year);
-		Collection<String> word = all_years.words();
-		for (String s: word) {
-			result.put(s, all_years.count(s));
-		}
-		return result;
-	}
+    /** Returns a defensive copy of the YearlyRecord of YEAR. */
+    public YearlyRecord getRecord(int year) {
+        YearlyRecord result = new YearlyRecord();
+        YearlyRecord all_years = years.get(year);
+        Collection<String> word = all_years.words();
+        for (String s: word) {
+            result.put(s, all_years.count(s));
+        }
+        return result;
+    }
 
-	/** Returns the total number of words recorded in all volumes. */
-	public TimeSeries<Long> totalCountHistory() {
-		TimeSeries<Long> result = new TimeSeries<Long>();
-		Collection<Number> years = counts.years();
-		Iterator<Number> yIter = years.iterator();
-		Collection<Number> data = counts.data();
-		Iterator<Number> dIter = data.iterator();
-		while (yIter.hasNext()) {
-			result.put(yIter.next().intValue(), dIter.next().longValue());
-		}
-	return result;
-	}
+    /** Returns the total number of words recorded in all volumes. */
+    public TimeSeries<Long> totalCountHistory() {
+        TimeSeries<Long> result = new TimeSeries<Long>();
+        Collection<Number> years = counts.years();
+        Iterator<Number> yIter = years.iterator();
+        Collection<Number> data = counts.data();
+        Iterator<Number> dIter = data.iterator();
+        while (yIter.hasNext()) {
+            result.put(yIter.next().intValue(), dIter.next().longValue());
+        }
+    return result;
+    }
 
-	/** Provides the history of WORD between STARTYEAR and ENDYEAR. */
-	public TimeSeries<Integer> countHistory(String word, int startYear, int endYear) {
-		TimeSeries<Integer> allYears = countHistory(word);
-		TimeSeries<Integer> result = new TimeSeries<Integer>(allYears, startYear, endYear);
-		return result;
-	}
+    /** Provides the history of WORD between STARTYEAR and ENDYEAR. */
+    public TimeSeries<Integer> countHistory(String word, int startYear, int endYear) {
+        TimeSeries<Integer> allYears = countHistory(word);
+        TimeSeries<Integer> result = new TimeSeries<Integer>(allYears, startYear, endYear);
+        return result;
+    }
 
-	/** Provides a defensive copy of the history of WORD. */
-	public TimeSeries<Integer> countHistory(String word) {
-		TimeSeries<Integer> result = new TimeSeries<Integer>();
-		Set<Integer> keys = years.keySet();
-		for (Integer i: keys) {
-			if (countInYear(word, i) != 0) {
-				result.put(i, countInYear(word, i));
-			}
-		}
-		return result;
-	}
+    /** Provides a defensive copy of the history of WORD. */
+    public TimeSeries<Integer> countHistory(String word) {
+        TimeSeries<Integer> result = new TimeSeries<Integer>();
+        Set<Integer> keys = years.keySet();
+        for (Integer i: keys) {
+            if (countInYear(word, i) != 0) {
+                result.put(i, countInYear(word, i));
+            }
+        }
+        return result;
+    }
 
-	/** Provides the relative frequency of WORD between STARTYEAR and ENDYEAR. */
-	public TimeSeries<Double> weightHistory(String word, int startYear, int endYear) {
-		TimeSeries<Double> weightHistory = weightHistory(word);
-		TimeSeries<Double> result = new TimeSeries<Double>(weightHistory, startYear, endYear);
-		return result;
-	}
+    /** Provides the relative frequency of WORD between STARTYEAR and ENDYEAR. */
+    public TimeSeries<Double> weightHistory(String word, int startYear, int endYear) {
+        TimeSeries<Double> weightHistory = weightHistory(word);
+        TimeSeries<Double> result = new TimeSeries<Double>(weightHistory, startYear, endYear);
+        return result;
+    }
 
-	/** Provides the relative frequency of WORD. */
-	public TimeSeries<Double> weightHistory(String word) {
-		TimeSeries<Integer> countHistory = countHistory(word);
-		TimeSeries<Double> result = countHistory.dividedBy(counts);
-		return result;
-	}
+    /** Provides the relative frequency of WORD. */
+    public TimeSeries<Double> weightHistory(String word) {
+        TimeSeries<Integer> countHistory = countHistory(word);
+        TimeSeries<Double> result = countHistory.dividedBy(counts);
+        return result;
+    }
 
-	/** Provides the summed relative frequency of all WORDS between
-	  * STARTYEAR and ENDYEAR. If a word does not exist, ignore it rather
-	  * than throwing an exception. */
-	public TimeSeries<Double> summedWeightHistory(Collection<String> words, 
-							  int startYear, int endYear) {
-		TimeSeries<Double> sum = summedWeightHistory(words);
-		TimeSeries<Double> result = new TimeSeries<Double>(sum, startYear, endYear);
-		return result;
-	}
-	/** Returns the summed relative frequency of all WORDS. */
-	public TimeSeries<Double> summedWeightHistory(Collection<String> words) {
-		TimeSeries<Double> result = new TimeSeries<Double>();
-		for (String t: words) {
-			TimeSeries<Double> weightHistory = weightHistory(t);
-			result = result.plus(weightHistory);
-		}
-		return result;
-	}
-	public TimeSeries<Double> processedHistory(int startYear, int endYear,
-											   YearlyRecordProcessor yrp) {
-		TimeSeries<Double> processed = processedHistory(yrp);
-		TimeSeries<Double> result = new TimeSeries<Double>(processed, startYear, endYear);
-		return result;
-	}
+    /** Provides the summed relative frequency of all WORDS between
+      * STARTYEAR and ENDYEAR. If a word does not exist, ignore it rather
+      * than throwing an exception. */
+    public TimeSeries<Double> summedWeightHistory(Collection<String> words, 
+                              int startYear, int endYear) {
+        TimeSeries<Double> sum = summedWeightHistory(words);
+        TimeSeries<Double> result = new TimeSeries<Double>(sum, startYear, endYear);
+        return result;
+    }
+    /** Returns the summed relative frequency of all WORDS. */
+    public TimeSeries<Double> summedWeightHistory(Collection<String> words) {
+        TimeSeries<Double> result = new TimeSeries<Double>();
+        for (String t: words) {
+            TimeSeries<Double> weightHistory = weightHistory(t);
+            result = result.plus(weightHistory);
+        }
+        return result;
+    }
+    public TimeSeries<Double> processedHistory(int startYear, int endYear,
+                                               YearlyRecordProcessor yrp) {
+        TimeSeries<Double> processed = processedHistory(yrp);
+        TimeSeries<Double> result = new TimeSeries<Double>(processed, startYear, endYear);
+        return result;
+    }
 
-	/** Provides processed history of all words ever as processed by YRP. */
-	public TimeSeries<Double> processedHistory(YearlyRecordProcessor yrp) {
-		TimeSeries<Double> result = new TimeSeries<Double>();
-		Set<Integer> keys = years.keySet();
-		for (Integer i: keys) {
-			double processed = yrp.process(years.get(i));
-			result.put(i, processed);
-		}
-		return result;
-	}
+    /** Provides processed history of all words ever as processed by YRP. */
+    public TimeSeries<Double> processedHistory(YearlyRecordProcessor yrp) {
+        TimeSeries<Double> result = new TimeSeries<Double>();
+        Set<Integer> keys = years.keySet();
+        for (Integer i: keys) {
+            double processed = yrp.process(years.get(i));
+            result.put(i, processed);
+        }
+        return result;
+    }
 
 }
--- proj1/ngordnet/TimeSeries.java
+++ proj1/ngordnet/TimeSeries.java
@@ -7,92 +7,92 @@ import java.util.Collection;
 import java.util.Iterator;
 
 public class TimeSeries<T extends Number> extends TreeMap<Integer, T> { 
-	/** Constructs a new empty TimeSeries. */
-	public TimeSeries() {
-	}
+    /** Constructs a new empty TimeSeries. */
+    public TimeSeries() {
+    }
 
-	/** Returns the years in which this time series is valid. Doesn't really
-	  * need to be a NavigableSet. This is a private method and you don't have 
-	  * to implement it if you don't want to. */
-	//private NavigableSet<Integer> validYears(int startYear, int endYear)
+    /** Returns the years in which this time series is valid. Doesn't really
+      * need to be a NavigableSet. This is a private method and you don't have 
+      * to implement it if you don't want to. */
+    //private NavigableSet<Integer> validYears(int startYear, int endYear)
 
-	/** Creates a copy of TS, but only between STARTYEAR and ENDYEAR. 
-	 * inclusive of both end points. */
-	public TimeSeries(TimeSeries<T> ts, int startYear, int endYear) {
-		for (int start = startYear; start < endYear + 1; start++) {
-			if (ts.containsKey(start)) {
-				T value = ts.get(start);
-				this.put(start, value);
-			}
-		}
-	}
+    /** Creates a copy of TS, but only between STARTYEAR and ENDYEAR. 
+     * inclusive of both end points. */
+    public TimeSeries(TimeSeries<T> ts, int startYear, int endYear) {
+        for (int start = startYear; start < endYear + 1; start++) {
+            if (ts.containsKey(start)) {
+                T value = ts.get(start);
+                this.put(start, value);
+            }
+        }
+    }
 
-	/** Creates a copy of TS. */
-	public TimeSeries(TimeSeries<T> ts) {
-		Collection<Number> years = ts.years();
-		for (Number n: years) {
-			Integer i = n.intValue();
-			this.put(i, ts.get(i));
-		}
-	}
+    /** Creates a copy of TS. */
+    public TimeSeries(TimeSeries<T> ts) {
+        Collection<Number> years = ts.years();
+        for (Number n: years) {
+            Integer i = n.intValue();
+            this.put(i, ts.get(i));
+        }
+    }
 
-	/** Returns the quotient of this time series divided by the relevant value in ts.
-	  * If ts is missing a key in this time series, return an IllegalArgumentException. */
-	public TimeSeries<Double> dividedBy(TimeSeries<? extends Number> ts) {
-		Set<Integer> keys = this.keySet();
-		for (Integer i: keys) {
-		   if (ts.containsKey(i) != true) {
-				  throw new IllegalArgumentException();
-				}
-			}
-		TimeSeries<Double> result = new TimeSeries<Double>();
-		for (Integer t: keys) {
-			if (ts.containsKey(t)) {
-				result.put(t, this.get(t).doubleValue()/ts.get(t).doubleValue());
-			}	
-		}
-		return result;
-	}
+    /** Returns the quotient of this time series divided by the relevant value in ts.
+      * If ts is missing a key in this time series, return an IllegalArgumentException. */
+    public TimeSeries<Double> dividedBy(TimeSeries<? extends Number> ts) {
+        Set<Integer> keys = this.keySet();
+        for (Integer i: keys) {
+           if (ts.containsKey(i) != true) {
+                  throw new IllegalArgumentException();
+                }
+            }
+        TimeSeries<Double> result = new TimeSeries<Double>();
+        for (Integer t: keys) {
+            if (ts.containsKey(t)) {
+                result.put(t, this.get(t).doubleValue()/ts.get(t).doubleValue());
+            }   
+        }
+        return result;
+    }
 
-	/** Returns the sum of this time series with the given ts. The result is a 
-	  * a Double time series (for simplicity). */
-	public TimeSeries<Double> plus(TimeSeries<? extends Number> ts) {
-		TimeSeries<Double> result = new TimeSeries<Double>();
-		Set<Integer> keys = this.keySet();
-		Set<Integer> key1 = ts.keySet();
-		for (Integer t: keys) {
-			if (ts.containsKey(t)) {
-				result.put(t, this.get(t).doubleValue() + ts.get(t).doubleValue());
-			}
-			else {
-				result.put(t, this.get(t).doubleValue());
-			}	
-		}
-		for (Integer i: key1) {
-			if (!result.containsKey(i)) {
-				result.put(i, ts.get(i).doubleValue());
-			}
-		}
-		return result;
-	}
+    /** Returns the sum of this time series with the given ts. The result is a 
+      * a Double time series (for simplicity). */
+    public TimeSeries<Double> plus(TimeSeries<? extends Number> ts) {
+        TimeSeries<Double> result = new TimeSeries<Double>();
+        Set<Integer> keys = this.keySet();
+        Set<Integer> key1 = ts.keySet();
+        for (Integer t: keys) {
+            if (ts.containsKey(t)) {
+                result.put(t, this.get(t).doubleValue() + ts.get(t).doubleValue());
+            }
+            else {
+                result.put(t, this.get(t).doubleValue());
+            }   
+        }
+        for (Integer i: key1) {
+            if (!result.containsKey(i)) {
+                result.put(i, ts.get(i).doubleValue());
+            }
+        }
+        return result;
+    }
 
-	/** Returns all years for this time series (in any order). */
-	public Collection<Number> years() {
-		Set<Integer> keys = this.keySet();
-		Collection<Number> x = new TreeSet<Number>();
-		x.addAll(keys);
-		return x;
-	}
+    /** Returns all years for this time series (in any order). */
+    public Collection<Number> years() {
+        Set<Integer> keys = this.keySet();
+        Collection<Number> x = new TreeSet<Number>();
+        x.addAll(keys);
+        return x;
+    }
 
-	/** Returns all data for this time series. 
-	  * Must be in the same order as years(). */
-	public Collection<Number> data() {
-		Iterator<Number> x = this.years().iterator();
-		Collection<Number> result = new LinkedList<Number>();
-		while (x.hasNext()) {
-			result.add(this.get(x.next()));
-		}
-		return result;
-	}
-	 
+    /** Returns all data for this time series. 
+      * Must be in the same order as years(). */
+    public Collection<Number> data() {
+        Iterator<Number> x = this.years().iterator();
+        Collection<Number> result = new LinkedList<Number>();
+        while (x.hasNext()) {
+            result.add(this.get(x.next()));
+        }
+        return result;
+    }
+     
 }
